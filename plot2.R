050df3e4f2aae201bea83a55fca1b7663b0d3509
lynnaevans <lynn@msa-inc.net>
2014-08-07 12:34:25 -0500

    First Plot 2 Commit

---
+++ plot2.R
@@ -0,0 +1,23 @@
+#
+# Read in full data set
+#
+fullDataSet <- read.csv("household_power_consumption.txt", sep=";", 
+                        na.strings="?", stringsAsFactors=FALSE)
+#
+# convert and concatenate date and time
+#
+fullDataSet$DateTime <- strptime(paste(fullDataSet$Date,fullDataSet$Time), 
+                                 "%d/%m/%Y %H:%M:%S")
+#
+# Generate a subset of the data for /02/01/2007 and 02/01/2007
+#
+ds<-subset(fullDataSet, as.Date(DateTime) == "2007-02-01" | 
+                   as.Date(DateTime) == "2007-02-02")
+#
+# Generate the histogram png file
+#
+png(file="plot2.png")
+plot(ds$DateTime,ds$Global_active_power,ylab="Global Active Power (kilowatts)",
+     xlab="", type = "l")
+mtext("Plot 2", line = 2, adj = 0)
+dev.off()
