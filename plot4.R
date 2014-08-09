63a3f0bc945cb541a508fc20651d54b8ec8389f0
lynnaevans <lynn@msa-inc.net>
2014-08-09 10:50:00 -0500

    First commit for plot 4

---
+++ plot4.R
@@ -0,0 +1,34 @@
+#
+# Read in full data set
+#
+fullDataSet <- read.csv("household_power_consumption.txt", sep=";", 
+                        na.strings="?", stringsAsFactors=FALSE)
+#
+# convert and concatenate date and time
+#
+fullDataSet$DateTime <- strptime(paste(fullDataSet$Date,fullDataSet$Time), 
+                                 "%d/%m/%Y %H:%M:%S")
+#
+# Generate a subset of the data for /02/01/2007 and 02/01/2007
+#
+ds<-subset(fullDataSet, as.Date(DateTime) == "2007-02-01" | 
+                   as.Date(DateTime) == "2007-02-02")
+#
+# Generate the png file
+#
+png(file="plot4.png")
+par(mfrow=c(2,2))
+plot(ds$DateTime,ds$Global_active_power,ylab="Global Active Power",
+     xlab="", type = "l")
+mtext("Plot 4", line = 2, adj = 0)
+plot(ds$DateTime,ds$Voltage,ylab="Global Active Power",
+     xlab="datetime", type = "l")
+plot(ds$DateTime,ds$Sub_metering_1,ylab="Energy sub metering",
+     xlab="", type = "l")
+points(ds$DateTime,ds$Sub_metering_2, col="red", type = "l")
+points(ds$DateTime,ds$Sub_metering_3, col="blue", type = "l")
+legend("topright",legend =c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col=c("black", "red", "blue"), lty=c(1,1,1))
+plot(ds$DateTime,ds$Global_reactive_power, ylab = "Global_reactive_power",
+     xlab="datetime", type = "l")
+dev.off()
