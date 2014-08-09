df378f36f4be6ffce56b9bfa7f890f33ab842740
lynnaevans <lynn@msa-inc.net>
2014-08-09 09:54:52 -0500

    First plot3.R commit

--- plot3.R
+++ plot3.R
@@ -16,8 +16,12 @@ ds<-subset(fullDataSet, as.Date(DateTime) == "2007-02-01" |
 #
 # Generate the png file
 #
-png(file="plot2.png")
-plot(ds$DateTime,ds$Global_active_power,ylab="Global Active Power (kilowatts)",
+png(file="plot3.png")
+plot(ds$DateTime,ds$Sub_metering_1,ylab="Energy sub metering",
      xlab="", type = "l")
-mtext("Plot 2", line = 2, adj = 0)
+points(ds$DateTime,ds$Sub_metering_2, col="red", type = "l")
+points(ds$DateTime,ds$Sub_metering_3, col="blue", type = "l")
+legend("topright",legend =c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col=c("black", "red", "blue"), lty=c(1,1,1))
+mtext("Plot 3", line = 2, adj = 0)
 dev.off()
